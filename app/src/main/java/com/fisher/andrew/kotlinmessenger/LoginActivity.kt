dba444f24e74ed12a3c9611ae7546c277f949e5f
Andrew Fisher <andrewfishersb@gmail.com>
2018-07-30 21:30:51 -0700

    Create layout for Main (register) and Login activity

---
+++ app/src/main/java/com/fisher/andrew/kotlinmessenger/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.fisher.andrew.kotlinmessenger
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+setContentView(R.layout.activity_login)
+
+
+
+
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/fisher/andrew/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/fisher/andrew/kotlinmessenger/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.fisher.andrew.kotlinmessenger
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        register_button.setOnClickListener {
+
+            val email = email_edit_text.text.toString()
+            val password = password_edit_text.text.toString()
+
+            Log.d("MainActivity","Email: $email Password: $password")
+
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
