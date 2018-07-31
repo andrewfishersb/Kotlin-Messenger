ab0b0249d6ea91d0d5b3033b4b6748771ffb7309
Andrew Fisher <andrewfishersb@gmail.com>
2018-07-30 21:39:52 -0700

    Complete functionality for retrieving edit text info and navigation for the two initial activities

--- app/src/main/java/com/fisher/andrew/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/fisher/andrew/kotlinmessenger/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.fisher.andrew.kotlinmessenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity(){
 
@@ -9,6 +10,14 @@ class LoginActivity: AppCompatActivity(){
         super.onCreate(savedInstanceState)
 setContentView(R.layout.activity_login)
 
+
+        login_button.setOnClickListener {
+            val email = login_email.text.toString()
+            val password = login_password.text.toString()
+
+        }
+
+        login_back_to_reg.setOnClickListener { finish() }
 
 
 
