2bd987e7dfcf12a20b1bbee975961e24d508a4f6
Andrew Fisher <andrewfishersb@gmail.com>
2018-07-30 22:23:39 -0700

    Import in Firebase resources and add functionality to create a user

--- app/src/main/java/com/fisher/andrew/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/fisher/andrew/kotlinmessenger/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.fisher.andrew.kotlinmessenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity(){
@@ -12,8 +14,9 @@ setContentView(R.layout.activity_login)
 
 
         login_button.setOnClickListener {
-            val email = login_email.text.toString()
-            val password = login_password.text.toString()
+
+            performLogin()
+
 
         }
 
@@ -23,7 +26,22 @@ setContentView(R.layout.activity_login)
 
     }
 
+    private fun performLogin(){
+        val email = login_email.text.toString()
+        val password = login_password.text.toString()
 
+        if (email.isEmpty() || password.isEmpty()) return
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+
+                }
+                .addOnFailureListener {
+                    Log.d("Main","Failed to create user: ${it.message}")
+                }
+    }
 
 
 
--- app/src/main/java/com/fisher/andrew/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/fisher/andrew/kotlinmessenger/MainActivity.kt
@@ -4,23 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+//TODO TURN ON INSTANT RUN?
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-
+//https://firebase.google.com/docs/auth/android/start/
 
         register_button.setOnClickListener {
+            performRegister()
 
-            val email = email_edit_text.text.toString()
-            val password = password_edit_text.text.toString()
-
-            Log.d("MainActivity","Email: $email Password: $password")
 
         }
 
@@ -29,5 +28,26 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun performRegister(){
+        val email = email_edit_text.text.toString()
+        val password = password_edit_text.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter text in email/pw",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("Main","Created user with uid: ${it.result.user.uid}")
+
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this,"Failed to create user: ${it.message}",Toast.LENGTH_SHORT).show()
+                }
     }
 }
